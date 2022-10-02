3ea6f1b85ac51d9d492be8d9118d0af4a7c32b4a
Rajesh Nayak <rajeshnayak2799@gmail.com>
2022-10-02 06:29:54 +0000

    Upload New File

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def buildJar(){
+    echo 'building the application...'
+    sh 'mvn package'
+}
+def buildApp(){
+    echo 'building the docker image...'
+    withCredentials([usernamePassword(credentialsId:'docker-hub-credentials',passwordVariable:'PASS',usernameVariable:'USER')]){
+    sh 'docker build -t rajeshnayak2799/demo-app:jma-2.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push rajeshnayak2799/demo-app:jma-2.0'
+}
+}
+
+def deployApp(){
+    echo "deploying the application..."
+} 
+
+return this
