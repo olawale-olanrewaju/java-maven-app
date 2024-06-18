df50fe3e2feaf1b91e7b887be5e12a272f4b1bf6
Olawale Olanrewaju <olanrewajume@yahoo.com>
2024-06-18 18:04:45 +0100

    Auto increment app version and docker image version

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,6 +15,19 @@ pipeline {
                 }
             }
         }
+        stage('Increment version') {
+            steps {
+                script {
+                    echo 'Incrementing application version'
+                    sh 'mvn build-helper:parse-version versions:set \
+                    -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+                    versions:commit'
+                    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+                    def version = matcher[0][1]
+                    env.IMAGE_VERSION = "$version-$BUILD_NUMBER"
+                }
+            }
+        }
         stage('Build Jar') {
             steps {
                 script {
--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    sh 'mvn clean package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-login', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t laweee/demo-java-maven-app:1.4 .'
+        sh "docker build -t laweee/demo-java-maven-app:$IMAGE_VERSION ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push laweee/demo-java-maven-app:1.4'
+        sh "docker push laweee/demo-java-maven-app:$IMAGE_VERSION"
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'Deploying application to EC2 instance'
 } 
 
 return this
